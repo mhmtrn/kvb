cdda0c3b31c77325bf117820c20b0a839d436fc4
Mahmut Turan <mturan@Mahmuts-MacBook-Pro.local>
2020-02-19 17:32:23 +0300

    update

--- kvb.py
+++ kvb.py
@@ -41,7 +41,6 @@ def turkish_ch(pdf):
     """
     The purpose of this function is update characters Turkish
     """
-
     pdf = pdf.replace("Ý", "İ")              
     pdf = pdf.replace("Ð", "Ğ")              
     pdf = pdf.replace("ý", "ı")              
@@ -70,10 +69,6 @@ def get_tax_id(pdf):
     return result
 
 
-
-    """
-    The purpose of this function is to detect Turkish tax id
-    """
 
 def get_name(pdf):
     """
@@ -108,7 +103,6 @@ def type_check(pdf):
     """
     The purpose of this function is to detect type of tax declaration
     """
-
     test1 = "KURUMLAR VERGİSİ BEYANNAMESİ"
     test2 = "GEÇİCİ VERGİ BEYANNAMESİ"
     
@@ -145,8 +139,6 @@ def get_term(pdf):
     """
     The purpose of this function is to get year of tax decalaration
     """
-
-    
     if pdf[:6] == "KURUML":      
         test = "l2"
         txt = ""
@@ -183,13 +175,46 @@ pdf = read_raw_pdf("kvb8.pdf")
 pdf = turkish_ch(pdf)
 
 print(get_tax_id(pdf))
-print(type_check(pdf)) # geçici vergide çalışmıyor
+print(type_check(pdf)) 
 print(get_name(pdf))
-print(get_term(pdf)) # geçici vergide çalışmıyor
+print(get_term(pdf)) 
 
 
+# DATABASE
+test_cases = ["kvb1.pdf","kvb2.pdf","kvb3.pdf","kvb4.pdf",
+              "kvb5.pdf","kvb6.pdf","kvb7.pdf","kvb8.pdf"]
 
 
+tax_id = []
+names = []
+terms = []
+types = []
+pdfs = []
+
+for i in test_cases:
+    pdf = read_raw_pdf(i)
+    pdf = turkish_ch(pdf)
+    pdfs.append(pdf)
+    
+    tax = get_tax_id(pdf)
+    tax_id.append(tax)
+    
+    type_form = type_check(pdf)
+    types.append(type_form)
+    
+    name = get_name(pdf)
+    names.append(name)
+    
+    term = get_term(pdf)
+    terms.append(term)
+    
+df = pd.DataFrame(list(zip(tax_id, types, names, terms, pdfs)), 
+               columns =['tax_id', 'types', 'names', 'terms', 'pdfs'])
 
 
-
+df.to_pickle("df")
+    
+    
+    
+    
+    
