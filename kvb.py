651175a1bbf921f13355c90e6db21d8a19f510b1
Mahmut Turan <mturan@Mahmuts-MacBook-Pro.local>
2020-02-19 13:50:44 +0300

    update

--- kvb.py
+++ kvb.py
@@ -7,7 +7,23 @@ Created on Wed Feb 19 11:49:04 2020
 """
 
 import pandas as pd
+import PyPDF2
 
+
+
+pdfFileObj = open('KVB_.pdf', 'rb')
+
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+length = pdfReader.numPages
+
+pdf = []
+for i in range(length):
+    pageObj = pdfReader.getPage(i) 
+    print(pageObj.extractText()) 
+    pdf.append(pageObj.extractText())
+
+pdf = ' '.join(pdf)
 
 
 
